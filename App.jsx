7e6e45a3c0d538d912fde3698a79f6dba62d6cea
agtanvi <ag.tanvi0709@gmail.com>
2026-06-18 01:04:01 +0530

    exp 8
    exp 8

--- App.jsx
+++ App.jsx
@@ -1,41 +1,11 @@
-import { useState } from "react";
-import "./App.css";
+import RegistrationForm from './pages/RegistrationForm'
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount((prev) => prev + 1);
-  };
-
-  const decrement = () => {
-    setCount((prev) => prev - 1);
-  };
-
-  const reset = () => {
-    setCount(0);
-  };
-
   return (
-    <main className="app-shell">
-      <section className="counter-card">
-        <p className="counter-label">Counter</p>
-        <h1>{count}</h1>
-
-        <div className="button-row">
-          <button className="btn decrement-btn" onClick={decrement}>
-            −
-          </button>
-          <button className="btn reset-btn" onClick={reset}>
-            Reset
-          </button>
-          <button className="btn increment-btn" onClick={increment}>
-            +
-          </button>
-        </div>
-      </section>
-    </main>
-  );
+    <>
+      <RegistrationForm />
+    </>
+  )
 }
 
-export default App;
+export default App
